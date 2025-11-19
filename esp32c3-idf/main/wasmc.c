b26092203677962755fbed5711bb888d6debb310
Luca <luca.orlandello@gmail.com>
2025-11-19 12:25:35 +0100

    On device compilation test

--- esp32c3-idf/main/wasmc.c
+++ esp32c3-idf/main/wasmc.c
@@ -1,10 +1,28 @@
 #include "all.h"
-#include "msort.h"
-
+//#include "qsort.h"
+//#include "msort.h"
+//#include "base64.h"
+//#include "matrix.h"
+#include "heapsort.h"
 
 void app_main(void)
 {
-    wasm_module *m = parse(msort_wasm, msort_wasm_len);
+    //unsigned char *start = test_qsort_wasm;
+    //unsigned int len = test_qsort_wasm_len;
+
+    //unsigned char *start = test_msort_wasm;
+    //unsigned int len = test_msort_wasm_len;
+
+    //unsigned char *start = test_base64_wasm;
+    //unsigned int len = test_base64_wasm_len;
+
+    //unsigned char *start = test_matrix_wasm;
+    //unsigned int len = test_matrix_wasm_len;
+
+    unsigned char *start = test_heapsort_wasm;
+    unsigned int len = test_heapsort_wasm_len;
+
+    wasm_module *m = parse(start, len);
     compile(m);
     free_wasm_module(m);
 }
